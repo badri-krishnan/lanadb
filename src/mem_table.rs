5c45f84884e3a7e1b767384a88f7a51cd9992562
Badri <badri12@gmail.com>
2022-03-11 14:50:19 -0500

    Unit test for the MemTable working

--- src/mem_table.rs
+++ src/mem_table.rs
@@ -2,7 +2,6 @@
 //We will write a duplicate to the WAL in case a failure in lanadb
 //There will be a max capacity to a MemTable at which point we will flust the table to the Disk
 //Entries will be stored in a HashMap 
-use std::ops::Index;
 
 pub struct MemTable{
     entries: Vec<Record>,
@@ -158,119 +157,116 @@ mod tests {
   
       assert_eq!(table.size, 148);
     }
-  
-    #[test]
+    #[test]  
     fn test_mem_table_put_end() {
-      let mut table = MemTable::new();
-      table.set(b"Apple", b"Apple Smoothie", 0);
-      table.set(b"Lime", b"Lime Smoothie", 10);
-  
-      table.set(b"Orange", b"Orange Smoothie", 20);
-  
-      assert_eq!(table.entries[0].key, b"Apple");
-      assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-      assert_eq!(table.entries[0].timestamp, 0);
-      assert_eq!(table.entries[0].deleted, false);
-      assert_eq!(table.entries[1].key, b"Lime");
-      assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Lime Smoothie");
-      assert_eq!(table.entries[1].timestamp, 10);
-      assert_eq!(table.entries[1].deleted, false);
-      assert_eq!(table.entries[2].key, b"Orange");
-      assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-      assert_eq!(table.entries[2].timestamp, 20);
-      assert_eq!(table.entries[2].deleted, false);
-  
-      assert_eq!(table.size, 108);
+        let mut table = MemTable::new();
+        table.set(b"Badri", b"Badri Krishnan", 10); // 5 + 14 + 16 + 1= 36
+        table.set(b"Lavanya", b"Lavanya Krishnan", 20); // 7 + 16 + 16 + 1 = 40
+    
+        table.set(b"Keerthi", b"Keerthi Krishnan", 30); // 7 + 16 + 16 + 1 = 40
+    
+        assert_eq!(table.entries[0].key, b"Badri");
+        assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Badri Krishnan");
+        assert_eq!(table.entries[0].timestamp, 10);
+        assert_eq!(table.entries[0].deleted, false);
+        assert_eq!(table.entries[1].key, b"Keerthi");
+        assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Keerthi Krishnan");
+        assert_eq!(table.entries[1].timestamp, 30);
+        assert_eq!(table.entries[1].deleted, false);
+        assert_eq!(table.entries[2].key, b"Lavanya");
+        assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Lavanya Krishnan");
+        assert_eq!(table.entries[2].timestamp, 20);
+        assert_eq!(table.entries[2].deleted, false);
+    
+        assert_eq!(table.size, 116);
+      }
+      #[test]  
+      fn test_mem_table_put_overwrite() {
+        let mut table = MemTable::new();
+        table.set(b"Badri", b"Badri Krishnan", 0); // 5 + 14 + 16 + 1= 36
+        table.set(b"Lavanya", b"Lavanya Krishnan", 10); // 7 + 16 + 16 + 1 = 40
+        table.set(b"Keerthi", b"Keerthi Krishnan", 20); // 7 + 16 + 16 + 1 = 40
+    
+    
+        table.set(b"Keerthi", b"Part of groomsmen", 30); //7 + 17 + 16 + 1 = 41
+    
+        assert_eq!(table.entries[0].key, b"Badri");
+        assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Badri Krishnan");
+        assert_eq!(table.entries[0].timestamp, 0);
+        assert_eq!(table.entries[0].deleted, false);
+        assert_eq!(table.entries[1].key, b"Keerthi");
+        assert_eq!(table.entries[1].value.as_ref().unwrap(), b"Part of groomsmen");
+        assert_eq!(table.entries[1].timestamp, 30);
+        assert_eq!(table.entries[1].deleted, false);
+        assert_eq!(table.entries[2].key, b"Lavanya");
+        assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Lavanya Krishnan");
+        assert_eq!(table.entries[2].timestamp, 10);
+        assert_eq!(table.entries[2].deleted, false);
+    
+        assert_eq!(table.size, 117);
+      }
+      #[test]
+      fn test_get_exists(){
+        let mut table = MemTable::new();
+        table.set(b"Badri", b"Badri Krishnan", 0); // 5 + 14 + 16 + 1= 36
+        table.set(b"Lavanya", b"Lavanya Krishnan", 10); // 7 + 16 + 16 + 1 = 40
+        table.set(b"Keerthi", b"Keerthi Krishnan", 20); // 7 + 16 + 16 + 1 = 40
+        
+        let record = table.get(b"Lavanya").unwrap();
+        assert_eq!(record.key, b"Lavanya");
+        assert_eq!(record.value.as_ref().unwrap(), b"Lavanya Krishnan");
+        assert_eq!(record.timestamp, 10);      
     }
-  
     #[test]
-    fn test_mem_table_put_overwrite() {
-      let mut table = MemTable::new();
-      table.set(b"Apple", b"Apple Smoothie", 0);
-      table.set(b"Lime", b"Lime Smoothie", 10);
-      table.set(b"Orange", b"Orange Smoothie", 20);
-  
-      table.set(b"Lime", b"A sour fruit", 30);
-  
-      assert_eq!(table.entries[0].key, b"Apple");
-      assert_eq!(table.entries[0].value.as_ref().unwrap(), b"Apple Smoothie");
-      assert_eq!(table.entries[0].timestamp, 0);
-      assert_eq!(table.entries[0].deleted, false);
-      assert_eq!(table.entries[1].key, b"Lime");
-      assert_eq!(table.entries[1].value.as_ref().unwrap(), b"A sour fruit");
-      assert_eq!(table.entries[1].timestamp, 30);
-      assert_eq!(table.entries[1].deleted, false);
-      assert_eq!(table.entries[2].key, b"Orange");
-      assert_eq!(table.entries[2].value.as_ref().unwrap(), b"Orange Smoothie");
-      assert_eq!(table.entries[2].timestamp, 20);
-      assert_eq!(table.entries[2].deleted, false);
-  
-      assert_eq!(table.size, 107);
+    fn test_get_failure(){
+        let mut table = MemTable::new();
+        table.set(b"Badri", b"Badri Krishnan", 0); // 5 + 14 + 16 + 1= 36
+        table.set(b"Lavanya", b"Lavanya Krishnan", 10); // 7 + 16 + 16 + 1 = 40
+        table.set(b"Keerthi", b"Keerthi Krishnan", 20); // 7 + 16 + 16 + 1 = 40
+        let record = table.get(b"Ryan");
+        assert_eq!(record.is_some(), false);
     }
-  
     #[test]
-    fn test_mem_table_get_exists() {
-      let mut table = MemTable::new();
-      table.set(b"Apple", b"Apple Smoothie", 0);
-      table.set(b"Lime", b"Lime Smoothie", 10);
-      table.set(b"Orange", b"Orange Smoothie", 20);
-  
-      let entry = table.get(b"Orange").unwrap();
-  
-      assert_eq!(entry.key, b"Orange");
-      assert_eq!(entry.value.as_ref().unwrap(), b"Orange Smoothie");
-      assert_eq!(entry.timestamp, 20);
+    fn test_delete_exists(){
+        let mut table = MemTable::new();
+        table.set(b"Badri", b"Badri Krishnan", 0); // 5 + 14 + 16 + 1= 36
+        table.set(b"Lavanya", b"Lavanya Krishnan", 10); // 7 + 16 + 16 + 1 = 40
+        table.set(b"Keerthi", b"Keerthi Krishnan", 20); // 7 + 16 + 16 + 1 = 40
+        
+        table.delete(b"Lavanya",40);
+
+        let record = table.get(b"Lavanya").unwrap();
+        assert_eq!(record.key, b"Lavanya");
+        assert_eq!(record.value, None);
+        assert_eq!(record.deleted, true);
+        assert_eq!(record.timestamp, 40); 
+
+        assert_eq!(table.entries[2].key, b"Lavanya");
+        assert_eq!(table.entries[2].value, None);
+        assert_eq!(table.entries[2].timestamp, 40);
+        assert_eq!(table.entries[2].deleted, true);
+
+        assert_eq!(table.size, 100);
     }
-  
-    #[test]
-    fn test_mem_table_get_not_exists() {
-      let mut table = MemTable::new();
-      table.set(b"Apple", b"Apple Smoothie", 0);
-      table.set(b"Lime", b"Lime Smoothie", 0);
-      table.set(b"Orange", b"Orange Smoothie", 0);
-  
-      let res = table.get(b"Potato");
-      assert_eq!(res.is_some(), false);
-    }
-  
-    #[test]
-    fn test_mem_table_delete_exists() {
-      let mut table = MemTable::new();
-      table.set(b"Apple", b"Apple Smoothie", 0);
-  
-      table.delete(b"Apple", 10);
-  
-      let res = table.get(b"Apple").unwrap();
-      assert_eq!(res.key, b"Apple");
-      assert_eq!(res.value, None);
-      assert_eq!(res.timestamp, 10);
-      assert_eq!(res.deleted, true);
-  
-      assert_eq!(table.entries[0].key, b"Apple");
-      assert_eq!(table.entries[0].value, None);
-      assert_eq!(table.entries[0].timestamp, 10);
-      assert_eq!(table.entries[0].deleted, true);
-  
-      assert_eq!(table.size, 22);
-    }
-  
+
     #[test]
     fn test_mem_table_delete_empty() {
-      let mut table = MemTable::new();
-  
-      table.delete(b"Apple", 10);
-  
-      let res = table.get(b"Apple").unwrap();
-      assert_eq!(res.key, b"Apple");
-      assert_eq!(res.value, None);
-      assert_eq!(res.timestamp, 10);
-      assert_eq!(res.deleted, true);
-  
-      assert_eq!(table.entries[0].key, b"Apple");
-      assert_eq!(table.entries[0].value, None);
-      assert_eq!(table.entries[0].timestamp, 10);
-      assert_eq!(table.entries[0].deleted, true);
-  
-      assert_eq!(table.size, 22);
+        let mut table = MemTable::new();
+    
+        table.delete(b"Badri", 10);
+    
+        let res = table.get(b"Badri").unwrap();
+        assert_eq!(res.key, b"Badri");
+        assert_eq!(res.value, None);
+        assert_eq!(res.timestamp, 10);
+        assert_eq!(res.deleted, true);
+    
+        assert_eq!(table.entries[0].key, b"Badri");
+        assert_eq!(table.entries[0].value, None);
+        assert_eq!(table.entries[0].timestamp, 10);
+        assert_eq!(table.entries[0].deleted, true);
+    
+        assert_eq!(table.size, 22);
     }
+
   }
